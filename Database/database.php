eee1699f09384aa753aa25c5ac642a1e8daf7dcf
DelrioRamirez4321 <delrioramirez7813@gmail.com>
2020-05-21 14:31:32 -0700

    remove data base and just put in main page

--- Database/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "tools";
-
-$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
--- tools.php
+++ tools.php
@@ -3,13 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="Stylesheet" href="CSS/main.css">        
-    <?php
-        include_once 'Database/database.php';
-    ?>
+    <link rel="Stylesheet" href="CSS/main.css">
     <title>Tools</title>
 </head>
 <body>
+
+<div id = "container">
     <header id = "header">
         <h1>HardWare Store</h1>
         <p>Being able to provide tools of any kind since 1992</p>
@@ -19,21 +18,25 @@
             <ul >
                 <li><a href="">Home</a></li>
                 <li><a href="" class = "active">Tools</a></li>
-                <li><a href="">stuff</a></li>
             </ul>
         </nav>
 
     <main id = "content">
         <h2>Tools for sale</h2>
         <p>All the tools that we have are both affordable and reliable</p>
-        <?php
-        $sql = "SELECT * from tools;";
+        <?php    
+        //connection to the localhost phpMyAdmin
+    $conn = mysqli_connect("localhost","root","","tools");
+
+        //telling what table we want from the local host
+        $sql = "SELECT * FROM `tools`";
         $result = mysqli_query($conn,$sql);
+        // checks numbers rows from tools tables
         $resultCheck = mysqli_num_rows($result);
 
-        echo '<th>ID</th> <th>Tool Name</th> <th> Brand</th> <th> Price</th> <th>IN Stock</th>';
-
+        //while loops to display all the data from the table until no data to display
         if($resultCheck > 0 ){
+            echo "<table id = 'tools_data'>";
             while ($row = mysqli_fetch_assoc($result)) {
                 $ID = $row['ID'];
                 $TOOLNAME = $row['TOOLNAME'];
@@ -41,24 +44,19 @@
                 $price = $row['price'];
                 $INSTOCK = $row['INSTOCK'];
 
-                echo '<tr>
-                <td>'.$ID.'</td>
-                <td>'.$TOOLNAME.'</td>
-                <td>'.$BRAND.'</td>
-                <td>'.$price.'</td>
-                <td>'.$INSTOCK.'</td>
-                </tr>
-                <br>
-                ';
-
+                echo  "<tr><td>" . $ID . "</td><td>". $TOOLNAME . "</td><td>"
+                 . $BRAND. "</td><td>" . $price . "</td><td>"  . $INSTOCK . "</td></tr>";
             }
+            echo "</table>";
         }
         ?>
 
     </main>
     <footer id = "footer">
-        <p id = "copyright"></p>
+        <p id = "copyright">Copyright 	&copy; Delrio Ramirez 2020</p>
     </footer>
+</div>
+
 </body>
 </html>
 
